5a7898dfcfbd3e741ff1d9a484b9ccc41985050f
huhubun <nongzc@outlook.com>
2019-08-07 23:49:13 +0800

    Import vue-ls.

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Storage from 'vue-ls'
 
 import { Button, Form, Input, Icon, Layout, Menu, Breadcrumb, List, Skeleton, Divider, Tag, Affix, message } from 'ant-design-vue'
 
@@ -12,6 +13,12 @@ import relativeTime from 'dayjs/plugin/relativeTime'
 import 'dayjs/locale/zh-cn'
 
 Vue.config.productionTip = false
+
+Vue.use(Storage, {
+  namespace: 'bunblog_ui__', // key prefix
+  name: 'sessionStorage', // name variable Vue.[ls] or this.[$ls],
+  storage: 'session' // storage name session, local, memory
+})
 
 Vue.use(Button)
 Vue.use(Form)
