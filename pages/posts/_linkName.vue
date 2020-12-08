926a7de3c17096c73efff9f3d92c1bad427799b9
huhubun <nongzc@outlook.com>
2020-12-09 01:16:41 +0800

    规范代码命名等 加入引言 修正页内导航在导航标题文字较少时，宽度不正确的问题

--- pages/posts/_linkName.vue
+++ pages/posts/_linkName.vue
@@ -1,15 +1,23 @@
 <template>
   <article>
     <header>
-      <v-card flat tile height="400" class="blue-grey darken-1 white--text">
-        <v-container style="height: 400px" class="d-flex flex-column">
+      <v-card
+        flat
+        tile
+        :height="blogPostHeaderHeight"
+        class="blue-grey darken-1 white--text"
+      >
+        <v-container
+          class="d-flex flex-column"
+          :style="{ height: `${blogPostHeaderHeight}px` }"
+        >
           <v-spacer />
 
-          <v-card-sub-title class="text-center">
+          <v-card-subtitle class="text-center" v-if="post.category">
             <v-chip small label color="blue-grey lighten-4">
               {{ post.category.displayName }}
             </v-chip>
-          </v-card-sub-title>
+          </v-card-subtitle>
 
           <v-card-title class="justify-center" :class="headerDisplayClass">
             {{ post.title }}
@@ -21,27 +29,17 @@
             <v-row>
               <v-col cols="6" class="py-0">
                 <v-chip
+                  v-for="headerIcon in headerIcons"
+                  :key="headerIcon.icon"
                   small
                   color="transparent"
                   class="blue-grey--text text--lighten-5"
                 >
                   <v-avatar left>
-                    <v-icon small>mdi-calendar-blank-outline</v-icon>
+                    <v-icon small>{{ headerIcon.icon }}</v-icon>
                   </v-avatar>
 
-                  {{ formatDate(post.publishedOn) }}
-                </v-chip>
-
-                <v-chip
-                  small
-                  color="transparent"
-                  class="blue-grey--text text--lighten-5"
-                >
-                  <v-avatar left>
-                    <v-icon small>mdi-eye-outline</v-icon>
-                  </v-avatar>
-
-                  {{ getVisits(post.metadataList) }}
+                  {{ headerIcon.text }}
                 </v-chip>
               </v-col>
               <v-col cols="6" class="text-right py-0">
@@ -68,7 +66,11 @@
         <!-- TODO 考虑在这里加个目录（默认折叠）供 < md 的屏幕尺寸查看；或者在返回顶部的按钮上面加个目录按钮 -->
 
         <!-- 博文内容 -->
-        <v-col cols="12" xl="10" lg="9" md="9" sm="12">
+        <v-col cols="12" xl="10" lg="9" md="9" sm="12" class="pt-0">
+          <blockquote>
+            <p>{{ post.excerpt }}</p>
+          </blockquote>
+
           <section
             v-highlight
             v-html="postContent"
@@ -79,11 +81,11 @@
         </v-col>
 
         <!-- 右侧页内导航 -->
-        <v-col cols="12" xl="2" lg="3" md="3" sm="0">
-          <div id="AnchorArea" class="d-none d-md-block">
+        <v-col cols="12" xl="2" lg="3" md="3" sm="0" id="AnchorContainer" class="pt-0">
+          <div id="AnchorArea" class="d-none d-md-block" :style="{width: `${anchorWidth}px`}">
             <v-navigation-drawer floating permanent width="100%">
               <v-list dense>
-                <v-list-item-group v-model="selectedItem">
+                <v-list-item-group v-model="anchorActiveItem">
                   <template v-for="anchor in anchors">
                     <v-list-item
                       :key="anchor.href"
@@ -146,15 +148,18 @@ export default {
     const converter = new showdown.Converter({ extensions: ['vue-img-lazy'] })
     const postContent = converter.makeHtml(post.content)
 
-    const anchors = []
-
-    return { post, postContent, anchors }
+    return { post, postContent }
   },
-  data: () => ({
-    showGoToTopButton: false,
-    selectedItem: null,
-    timeout: null
-  }),
+  data() {
+    return {
+      blogPostHeaderHeight: 400,
+      anchors: [],
+      anchorWidth: 0,
+      showGoToTopButton: false,
+      anchorActiveItem: null,
+      anchorActiveItemTimeoutId: null
+    }
+  },
   methods: {
     formatDate(datetime) {
       let dayjsObj = dayjs(datetime)
@@ -170,27 +175,29 @@ export default {
 
       return 0
     },
-    onScroll() {
+    onScrollYGreaterThan400() {
       // TODO 考虑用指令实现？（问题：如何在指令里绑定和移除事件？）
+
+      let anchorAreaElement = document.querySelector('#AnchorArea')
 
       if (window.scrollY >= 400) {
         // 右侧页内导航栏浮动效果
-        document.querySelector('#AnchorArea').classList.add('fixed-anchor')
+        anchorAreaElement.classList.add('fixed-anchor')
 
         // 返回顶部按钮
         this.showGoToTopButton = true
       } else {
-        document.querySelector('#AnchorArea').classList.remove('fixed-anchor')
+        anchorAreaElement.classList.remove('fixed-anchor')
         this.showGoToTopButton = false
       }
     },
-    onScroll2() {
+    onScrollUpdateAnchorActiveItem() {
       // 页内导航高亮
-      if (this.timeout != null) {
-        clearTimeout(this.timeout)
+      if (this.anchorActiveItemTimeoutId != null) {
+        clearTimeout(this.anchorActiveItemTimeoutId)
       }
 
-      this.timeout = setTimeout(() => {
+      this.anchorActiveItemTimeoutId = setTimeout(() => {
         let currentAnchor = null
         let scrollY = Math.ceil(window.scrollY)
 
@@ -220,11 +227,15 @@ export default {
         }
 
         if (currentAnchor) {
-          this.selectedItem = currentAnchor.href
+          this.anchorActiveItem = currentAnchor.href
         } else {
-          this.selectedItem = null
+          this.anchorActiveItem = null
         }
       }, 100)
+    },
+    onResizeUpdateAnchorWidth(){
+      // 24 = container 的 padding，左右各 12px
+      this.anchorWidth = document.querySelector('#AnchorContainer').clientWidth - 24
     }
   },
   head() {
@@ -259,6 +270,18 @@ export default {
         case 'xl':
           return 'display-3'
       }
+    },
+    headerIcons: function () {
+      return [
+        {
+          icon: 'mdi-calendar-blank-outline',
+          text: this.formatDate(this.post.publishedOn)
+        },
+        {
+          icon: 'mdi-eye-outline',
+          text: this.getVisits(this.post.metadataList)
+        }
+      ]
     }
   },
   mounted() {
@@ -266,12 +289,16 @@ export default {
       document.querySelector('.blog-post-content')
     )
 
-    window.addEventListener('scroll', this.onScroll)
-    window.addEventListener('scroll', this.onScroll2)
+    this.onResizeUpdateAnchorWidth()
+
+    window.addEventListener('scroll', this.onScrollYGreaterThan400)
+    window.addEventListener('scroll', this.onScrollUpdateAnchorActiveItem)
+    window.addEventListener('resize', this.onResizeUpdateAnchorWidth)
   },
   destroyed() {
-    window.removeEventListener('scroll', this.onScroll)
-    window.removeEventListener('scroll', this.onScroll2)
+    window.removeEventListener('scroll', this.onScrollYGreaterThan400)
+    window.removeEventListener('scroll', this.onScrollUpdateAnchorActiveItem)
+    window.removeEventListener('resize', this.onResizeUpdateAnchorWidth)
   },
   components: {
     CreativeCommons
