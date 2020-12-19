4addbd6252a90f1f400e3cd524f976901df9cb4f
huhubun <nongzc@outlook.com>
2020-12-20 00:19:46 +0800

    为新的首页增加 head 头的内容

--- pages/index.vue
+++ pages/index.vue
@@ -86,6 +86,25 @@ export default {
         }
       ]
     }
+  },
+  head() {
+    return {
+      title: '首页',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content:
+            '欢迎访问呼呼小笼包 (huhubun) 的个人博客。这里的文章包含了前后端软件开发技术博文、精品软件推荐等内容。欢迎大家一起学习、成长。也欢迎关注微信公众号 bundev 交流。'
+        },
+        {
+          hid: 'keywords',
+          name: 'keywords',
+          content:
+            '呼呼小笼包,huhubun,bun,bun.dev,bun.plus,技术,软件,开发,个人博客,博客,文章,精品软件,推荐'
+        }
+      ]
+    }
   }
 }
 </script>
